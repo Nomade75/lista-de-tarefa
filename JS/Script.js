9516c9ba0a2765e4f8da804b5f68861af144a45d
Nomade <josefernamdeslage@gmail.com>
2023-09-11 15:49:16 -0300

    projeto pronto

--- JS/Script.js
+++ JS/Script.js
@@ -11,8 +11,9 @@ function acoesNaPagina(){
 document.addEventListener('click',(element)=>{
     let elementoClicado = element.target;
     if(elementoClicado.classList.contains('button')){   
-
+        if(!inputText.value) return
         criarTarefa(inputText.value);
+        criarLocalStorage()
         limparCampo()  
         
     }
@@ -21,7 +22,9 @@ document.addEventListener('click',(element)=>{
 
 document.addEventListener('keypress',(e)=>{
     if(e.keyCode == 13){
+        if(!inputText.value) return
         criarTarefa(inputText.value);
+        criarLocalStorage()
         limparCampo() 
 
     }
@@ -32,6 +35,7 @@ document.addEventListener('click',(e)=>{
     let elemento = e.target;
     if(elemento.classList.contains('delete')){
         elemento.parentElement.remove();
+        criarLocalStorage()
         
     }
     //funcao que remove o iten da lista com base em seu elemento pai
@@ -48,17 +52,46 @@ function criarTarefa(tarefa){
     let containerTarefa = document.querySelector('.lista');
     let itenLista = criarItenLista();
     itenLista.innerText = tarefa;
-    criarBotaoDelete(itenLista)
     containerTarefa.appendChild(itenLista);
+    criarBotaoDelete(itenLista)
     //cria a tarefa
 }
 
 function criarBotaoDelete(itenLista){
-    let itenDaLista = itenLista;
-    let botaoDelete = document.createElement('button');
-    botaoDelete.setAttribute('class','delete')
-    botaoDelete.innerText = 'Apagar';
-    itenDaLista.innerText += ' '
-    itenDaLista.appendChild(botaoDelete);
+   itenLista.innerText += ' ';
+   let criarBotaoDelete = document.createElement('button');
+   criarBotaoDelete.innerText = "Apagar";
+   criarBotaoDelete.setAttribute('class','delete');
+   itenLista.appendChild(criarBotaoDelete);
     //cria um botao de apagar junto da tarefa
 }
+
+function criarLocalStorage(){
+    const tarefas = document.querySelector('.lista');
+    const itenDeLista = tarefas.querySelectorAll('li');
+    let colecaoDeTarefas = [];
+
+    for(tarefa of itenDeLista){
+      let text = tarefa.innerText;
+      text = text.replace('APAGAR','').trim()
+      colecaoDeTarefas.push(text)
+    }
+    const taskToJson = JSON.stringify(colecaoDeTarefas);
+    localStorage.setItem('tarefas',taskToJson);
+
+    //aqui e criado uma funcao que pega os valores inseridos na lista de tarefas os coloca num array
+    //e depois os transforma numa string JSON para poder ser guardado no localStorage do navegador
+    //e assim poder ter acesso a eles mesmo que o navegador seja fechado
+}
+
+function recolocarTarefas(){
+    let tasks = localStorage.getItem('tarefas');
+    let taskList = JSON.parse(tasks);
+
+    for(tarefa of taskList){
+        criarTarefa(tarefa);
+    }
+
+    //aqui as tarefas salvas sao recolocadas na lista
+    //funcionando da mesma forma podendo remover ou adicionar novas
+}recolocarTarefas();
